834d6bbfc65b0d8bde4302bfc2d8444d9e8fed14
hackezar <107069171+hackezar@users.noreply.github.com>
2024-03-06 17:21:32 -0800

    V1.0, Might find minor bugs in future

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-let url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=cats';
+let sliderValue = document.getElementById('myRange').value;
+let url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=cats&weirdness=' + sliderValue;
 async function getGif(url) {
+    console.log(url);
     const img = document.getElementById('image');
+    if (img.src != '#') {
+        img.src = '#';
+    }
     try {
     await fetch(url, {mode: 'cors'})
       .then(function(response) {
@@ -13,8 +18,11 @@ async function getGif(url) {
       .then(function(response) {
         try {
         img.src = response.data.images.original.url;
+        console.log(img.src);
         } catch(errors) {
+            console.log(document.getElementById('search').value);
             console.log(errors);
+            getGif('https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=cats&weirdness=' + document.getElementById('myRange').value);
         }
       });
     } catch(response) {
@@ -22,25 +30,118 @@ async function getGif(url) {
     }
 };
 
-getGif();
-
-// Event Listener for button
-document.getElementById('btn').addEventListener('click', function eventHandler() {
+let eventHandler = function () {
+    console.log('button clicked');
     getGif(url);
-});
+}
+
+let eventHandler2 = function(){
+    try {
+        let input = document.getElementById('search').value;
+        let sliderValue = document.getElementById('myRange').value;
+        url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=' + input + '&weirdness=' + sliderValue; 
+        getGif(url);
+        document.getElementById('btn').removeEventListener('click', eventHandler);
+        document.getElementById('btn').addEventListener('click', eventHandler);
+    } catch(errors) {
+        console.log(errors);
+    }
+}
+// Event Listener for button
+document.getElementById('btn').addEventListener('click', eventHandler);
 
 // Event listener for search bar
-document.getElementById('searchIcon').addEventListener('click', function eventHandler2() {
-    try {
-    let input = document.getElementById('search');
-    let url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=' + input;
-    getGif(url);
-    document.getElementById('btn').removeEventListener('click', eventHandler);
-    document.getElementById('btn').addEventListener('click', function eventHandler() {
-        getGif();
-        this.removeEventListener('click', eventHandler);
-    });
-    } catch(error) {
-        console.log('error');
-    };
-});
+document.getElementById('searchIcon').addEventListener('click', eventHandler2);
+
+//slider info. Change number for how weird the slider starts at
+let slideValue = 1;
+document.getElementById('myRange').value = slideValue;
+document.getElementById('slideOutput').innerHTML = slideValue;
+//Update output for slider when value is changed
+document.getElementById('myRange').oninput = function() {
+    switch(this.value) {
+        case "1":
+            slideOutput.innerHTML = 1;
+            if (document.getElementById('search').value == "") {
+                url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=cats&weirdness=' + this.value; 
+            }
+            url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=' + document.getElementById('search').value + '&weirdness=' + this.value;
+            getGif(url);
+            break;
+        case "2":
+            slideOutput.innerHTML = 2;
+            if (document.getElementById('search').value) {
+                url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=cats&weirdness=' + this.value; 
+            }
+            url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=' + document.getElementById('search').value + '&weirdness=' + this.value;
+            getGif(url);
+            break;
+        case "3":
+            slideOutput.innerHTML = 3;
+            if (document.getElementById('search').value === "") {
+                url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=cats&weirdness=' + this.value; 
+            }
+            url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=' + document.getElementById('search').value + '&weirdness=' + this.value;
+            getGif(url);
+            break;
+        case "4":
+            slideOutput.innerHTML = 4;
+            if (document.getElementById('search').value === "") {
+                url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=cats&weirdness=' + this.value; 
+            }
+            url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=' + document.getElementById('search').value + '&weirdness=' + this.value;
+            getGif(url);
+            break;
+        case "5":
+            slideOutput.innerHTML = 5;
+            if (document.getElementById('search').value === "") {
+                url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=cats&weirdness=' + this.value; 
+            }
+            url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=' + document.getElementById('search').value + '&weirdness=' + this.value;
+            getGif(url);
+            break;
+        case "6":
+            slideOutput.innerHTML = 6;
+            if (document.getElementById('search').value === "") {
+                url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=cats&weirdness=' + this.value; 
+            }
+            url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=' + document.getElementById('search').value + '&weirdness=' + this.value;
+            getGif(url);
+            break;
+        case "7":
+            slideOutput.innerHTML = 7;
+            if (document.getElementById('search').value === "") {
+                url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=cats&weirdness=' + this.value; 
+            }
+            url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=' + document.getElementById('search').value + '&weirdness=' + this.value;
+            getGif(url);
+            break;
+        case "8":
+            slideOutput.innerHTML = 8;
+            if (document.getElementById('search').value === "") {
+                url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=cats&weirdness=' + this.value; 
+            }
+            url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=' + document.getElementById('search').value + '&weirdness=' + this.value;
+            getGif(url);
+            break;
+        case "9":
+            slideOutput.innerHTML = 9;
+            if (document.getElementById('search').value === "") {
+                url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=cats&weirdness=' + this.value; 
+            }
+            url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=' + document.getElementById('search').value + '&weirdness=' + this.value;
+            getGif(url);
+            break;
+        case "10":
+            slideOutput.innerHTML = 10;
+            if (document.getElementById('search').value === "") {
+                url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=cats&weirdness=' + this.value; 
+            }
+            url = 'https://api.giphy.com/v1/gifs/translate?api_key=yuMrTYXhejsKm76cHucXyCafcb5tEyo6&s=' + document.getElementById('search').value + '&weirdness=' + this.value;
+            getGif(url);
+            break;
+    }
+}
+
+
+getGif(url);
